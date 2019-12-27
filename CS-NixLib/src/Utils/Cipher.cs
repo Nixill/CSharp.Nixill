c5eb10129b30bca02ca90590146207188aea9b69
ShadowFoxNixill <github@nix.nixill.net>
2019-12-26 19:07:33 -0500

    More work on Cipher (mostly docs)

--- CS-NixLib/src/Utils/Cipher.cs
+++ CS-NixLib/src/Utils/Cipher.cs
@@ -2,10 +2,27 @@ using System;
 using System.Collections.Generic;
 
 namespace Nixill.Utils {
+  /// <summary>
+  /// Represents a simple one-way substitution cipher.
+  /// 
+  /// Instances of the Cipher class can be used for multi-character substitution including swaps and cycles.
+  /// </summary>
   public class Cipher {
     Dictionary<char, string> CharMap;
+    
+    /// <summary>
+    /// The string representing the characters being substituted.
+    /// </summary>
     public string Source { get; }
+
+    /// <summary>
+    /// The string representing the substitute characters.
+    /// </summary>
     public string Target { get; }
+
+    /// <summary>
+    /// Whether or not this is a reversible cipher.
+    /// </summary>
     public bool Reversible { get; }
 
     public Cipher(string src, string trg) {
