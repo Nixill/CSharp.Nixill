433b15320f7d3caed90ab66e89b50b7fdb7e5b38
ShadowFoxNixill <github@nix.nixill.net>
2020-01-02 14:01:25 -0500

    Initial work on CSVParser

---
+++ CS-NixLib/src/Grid/CSVParser.cs
@@ -0,0 +1,10 @@
+namespace Nixill.Tables.CSV {
+  public class CSVParser {
+    public static Grid<string> FileToGrid(File input, bool emptyStrings = true) {
+      // get iterator of strings
+    }
+    
+    public static Grid<string> StringToGrid(string input, bool emptyStrings = true) {
+    }
+  }
+}
