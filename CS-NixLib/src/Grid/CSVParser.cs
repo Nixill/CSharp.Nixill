ea1889d4f750e853214195c7ce1798f275730d3c
ShadowFoxNixill <github@nix.nixill.net>
2020-01-03 17:39:38 -0500

    ready for block war

--- CS-NixLib/src/Grid/CSVParser.cs
+++ CS-NixLib/src/Grid/CSVParser.cs
@@ -11,7 +11,7 @@ namespace Nixill.Grid.CSV {
       // iterate through lines of string
     }
 
-    public static Grid<string> EnumeratorToGrid(IEnumerable<string> input, bool emptyStrings = true) {
+    public static Grid<string> EnumerableToGrid(IEnumerable<char> input, bool emptyStrings = true) {
       List<List<string>> backingList = new List<List<string>>();
       List<string> innerList = new List<string>();
       StringBuilder val = new StringBuilder();
@@ -20,11 +20,36 @@ namespace Nixill.Grid.CSV {
       bool lastIsCR = false;
       bool lastIsDQ = false;
 
-      foreach (string str in input) {
-        foreach (char chr in str) {
-          if (chr == '"') {
-
+      foreach (char chr in input) {
+        if (chr == '"') {
+          if (inQuotes) {
+            inQuotes = false;
+            lastIsDQ = true;
           }
+          else {
+            if (lastIsDQ) {
+              val.Append('"');
+              lastIsDQ = false;
+            }
+            inQuotes = true;
+          }
+          lastIsCR = false;
+          isEmptyLine = false;
+        }
+        else if (inQuotes) {
+          val.Append(chr);
+          lastIsDQ = false;
+          lastIsCR = false;
+        }
+        else if (chr == ',') {
+          innerList.Add(val.ToString());
+          val.Clear();
+          lastIsDQ = false;
+          lastIsCR = false;
+          isEmptyLine = false;
+        }
+        else if (chr == '\r') {
+
         }
       }
     }
