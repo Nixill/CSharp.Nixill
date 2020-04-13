16da0a57a6c95faceaf4e3b53a564c5e4de14cfc
ShadowFoxNixill <github@nix.nixill.net>
2020-04-13 15:58:11 -0400

    Clear quicktests

--- CS-NixLib-Test/src/QuickTests.cs
+++ CS-NixLib-Test/src/QuickTests.cs
@@ -1,16 +1,12 @@
 using System.IO;
 using System;
 using NUnit.Framework;
-using Nixill.Collections.Grid;
-using Nixill.Collections.Grid.CSV;
 using System.Collections.Generic;
 
 namespace Nixill.Test {
   public class QuickTests {
     [Test]
     public void QuickTest() {
-      int i = Int32.MaxValue;
-      Console.WriteLine(i + 1);
     }
   }
 }
