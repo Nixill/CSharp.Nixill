921bf0fe69299c9045f5f62aafc0b005deb78b26
Steven <github@steven-h.net>
2023-12-13 02:34:29 -0500

    EnumerableUtils.Combinations()

--- CSharp.Nixill/src/Utils/EnumerableUtils.cs
+++ CSharp.Nixill/src/Utils/EnumerableUtils.cs
@@ -418,6 +418,45 @@ public static class EnumerableUtils
       elemList.Add(elem);
     }
   }
+
+  // "limit" is non-optional here because 0 would just reutrn basically
+  // the original list.
+  //
+  // Also, this immediately enumerates everything because otherwise it
+  // would carry a risk of multiple enumeration.
+  public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elems, int limit)
+  {
+    T[] elemArray = elems.ToArray();
+
+    if (limit <= 0) limit += elemArray.Length;
+    if (limit < 0) return Enumerable.Empty<IEnumerable<T>>();
+    if (limit >= elemArray.Length) return EnumerableUtils.Of(elems);
+
+    return Combs(elemArray, limit);
+  }
+
+  static IEnumerable<IEnumerable<T>> Combs<T>(IEnumerable<T> elemList, int keep)
+  {
+    int skips = elemList.Count() - keep;
+    if (skips == 0)
+    {
+      yield return elemList;
+      yield break;
+    }
+
+    foreach ((T elem, int i) in elemList.Take(skips + 1).WithIndex())
+    {
+      if (keep == 1) yield return EnumerableUtils.Of(elem);
+      else
+      {
+        foreach (IEnumerable<T> comb in Combs(elemList.Skip(i + 1), keep - 1))
+        {
+          yield return comb.Prepend(elem);
+        }
+      }
+    }
+  }
+
   public static IEnumerable<T> Repeat<T>(this IEnumerable<T> seq, int count)
   {
     foreach (int i in Enumerable.Range(0, count))
